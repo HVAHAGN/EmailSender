04e5cfb0c735f9f170bf35c9550332fd90c998dc
hvahagn <vhoagamo@gmail.com>
2020-07-18 13:11:14 +0400

    add encryptor

---
+++ TestForEmail/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a password to use:");
+            string password = Console.ReadLine();
+            Console.WriteLine("Please enter a string to encrypt:");
+            string plaintext = Console.ReadLine();
+            Console.WriteLine("");
+
+            Console.WriteLine("Your encrypted string is:");
+            string encryptedstring = StringCipher.Encrypt(plaintext, password);
+            Console.WriteLine(encryptedstring);
+            Console.WriteLine("");
+
+            Console.WriteLine("Your decrypted string is:");
+            string decryptedstring = StringCipher.Decrypt(encryptedstring, password);
+            Console.WriteLine(decryptedstring);
+            Console.WriteLine("");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+
+        }
+
+
+        public static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(inputString))
+            {
+                sb.Append(b.ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+        public static byte[] GetHash(string inputString)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+    }
+}
